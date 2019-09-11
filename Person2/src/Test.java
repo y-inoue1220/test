9a903a4ece0216460de8eebb49457fd74e7baadf
Rush-light <lightning_o-din0711@docomo.ne.jp>
2019-09-11 21:11:45 +0900

    911

--- Person2/src/Test.java
+++ Person2/src/Test.java
@@ -1,6 +1,7 @@
 
 public class Test {
 	public static void main(String[]args){
+
 		Person taro=new Person();
 		taro.name="taro";
 		taro.age=18;
@@ -10,6 +11,18 @@ public class Test {
 		Person jiro=new Person("jiro",20);
 		System.out.println(jiro.name);
 		System.out.println(jiro.age);
+
+		Person saburo=new Person("saburo",0);
+		System.out.println(saburo.name);
+		System.out.println(saburo.age);
+
+		Person 名前なし=new Person("名前なし",25);
+		System.out.println(名前なし.name);
+		System.out.println(名前なし.age);
+
+		Person hanako=new Person("hanako",17);
+		System.out.println(hanako.name);
+		System.out.println(hanako.age);
 	}
 }
 
