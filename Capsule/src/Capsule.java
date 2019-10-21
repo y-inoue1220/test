c9a30781807d9bad7633f3617161a7f491dc48b5
Rush-light <lightning_o-din0711@docomo.ne.jp>
2019-10-21 20:43:40 +0900

    1021

--- Capsule/src/Capsule.java
+++ Capsule/src/Capsule.java
@@ -14,19 +14,6 @@ public class Capsule {
 
 
 
-public class Capsule {
-	public static void main(String[]args){
-		Person taro = new Person("山田太郎",20);
-		System.out.println(taro.getName());
-		taro.setName("花子");
-		System.out.println(taro.getName());
-		System.out.println(taro.getAge());
-		taro.setAge(30);
-		System.out.println(taro.getAge());
-	}
-
-
-}
 
 
 >>>>>>> be73979b0219f0765b0446127f80ab50ebf5dab2
--- TestProject01/src/HelloWorld.java
+++ TestProject01/src/HelloWorld.java
@@ -4,5 +4,5 @@ public class HelloWorld {
 	public static void main(String[] args) {
 
 System.out.println("こんにちは！！");
-	｝
+	}
 }
